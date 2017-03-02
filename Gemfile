809e81ce74cb66d2f080f3eeeccbf09d14b4bf71
William Godard <grenier.godard@gmail.com>
2017-03-02 10:42:07 +0100

    Add some config

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
+
+ruby "2.3.1"
+
+gem "puma"
 gem 'middleman', '~> 3.4'
+gem 'rack-contrib', '~> 1.4'
 
 # Deploy to GitHub Pages
 gem 'middleman-deploy', '~> 1.0'
--- config.rb
+++ config.rb
@@ -84,8 +84,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
 
   # Optional Settings
-  deploy.remote = 'heroku' # remote name or git url, default: origin
-  deploy.branch = 'master' # default: gh-pages
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
